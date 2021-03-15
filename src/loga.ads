74486bb8197303e42c1cabfd9c74e718eafa891b
Cyrill Korshnyak <kirillkorshnyak@yandex.ru>
2021-03-15 23:08:45 +1000

    Stable (i hope) version, but need some revisions

--- src/loga.ads
+++ src/loga.ads
@@ -9,20 +9,20 @@ private
    type Colors is (Normal, Red, Green, Yellow, Blue, Magenta, Cyan);
 
    Colors_As_Integer : array (Colors) of Natural := (Normal  => 0,
-                                                     Red => 31,
-                                                     Green => 32,
-                                                     Yellow => 33,
-                                                     Blue => 34,
+                                                     Red     => 31,
+                                                     Green   => 32,
+                                                     Yellow  => 33,
+                                                     Blue    => 34,
                                                      Magenta => 35,
-                                                     Cyan => 36);
+                                                     Cyan    => 36);
    type Logger is tagged record
       Name     : Unbounded_String;
       Color    : Colors;
       Disabled : Boolean := True;
    end record;
 
-   Current_Color : Natural range Colors_As_Integer (Colors'Val (1))
-      .. Colors_As_Integer (Colors'Last) := Colors_As_Integer (Red);
+   Current_Color : Natural range Colors_As_Integer (Colors'Val (1)) ..
+      Colors_As_Integer (Colors'Last) := Colors_As_Integer (Red);
 
    procedure Colorize_Output (Color : Colors);
    function Start_Bold return String;
